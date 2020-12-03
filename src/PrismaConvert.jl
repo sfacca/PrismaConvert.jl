d2bf45c56180beded342e1bd5750f7645ec135ca
Sebastiano Facca <830479@stud.unive.it>
2020-12-03 15:41:06 +0100

    linked fs

--- src/PrismaConvert.jl
+++ src/PrismaConvert.jl
@@ -3,9 +3,19 @@ module PrismaConvert
 
 
 include("maketif/eos_convert.jl")
-export maktif
+include("HDF5filesDict/HDF5filesDict.jl")
+export maketif, open, close
 
 maketif = eos_convert.maketif
+
+function open(f)
+    open(f,"r")
+end
+function open(f,mode::String)
+    HDF5fd.filesDict(f,mode)
+end
+
+close = HDF5fd.closeall
 
 
 end # module
