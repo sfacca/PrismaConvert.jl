80396e51585aa071916c225bd73a90ef5ba22274
Sebastiano Facca <830479@stud.unive.it>
2021-01-05 20:54:03 +0100

    updated dependencies

--- docs/make.jl
+++ docs/make.jl
@@ -16,7 +16,24 @@ include("../src/PrismaConvert.jl")
 #pop!(LOAD_PATH)
 
 # ╔═╡ ab6b5e60-387d-11eb-32c1-69d96662e61e
-makedocs(sitename="Prisma Convert")
+    makedocs(;
+    modules=[PrismaConvert],
+    repo="https://github.com/sfacca/PrismaConvert.jl/blob/{commit}{path}#L{line}",
+    sitename="PrismaConvert.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://sfacca.github.io/PrismaConvert.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md"
+    ],
+    )
+
+deploydocs(;
+    repo = "github.com/sfacca/PrismaConvert.jl.git",
+    push_preview=true,
+)
 
 # ╔═╡ Cell order:
 # ╠═a4a55c20-387d-11eb-320a-5ba6a364fca7
